faf7aa90beb042eea77e4f19e2642f1a76222555
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2026-01-31 16:25:25 +0700

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,10 @@ export default function App() {
         "group/body overscroll-none antialiased [--footer-height:calc(var(--spacing)*14)] [--header-height:calc(var(--spacing)*14)] xl:[--footer-height:calc(var(--spacing)*24)]",
       )}
     >
-      <div data-slot="layout" className="bg-background relative z-10 flex min-h-svh flex-col">
+      <div className="bg-background relative z-10 flex min-h-svh flex-col">
         <main className="flex flex-1 flex-col">
           <div className="container-wrapper flex flex-1 flex-col px-2 py-4 lg:py-20">
-            <div className="mx-auto my-0 py-0 px-4 lg:px-32 max-w-5xl">
+            <div className="mx-auto flex w-full max-w-160 min-w-0 flex-1 flex-col gap-6 px-4 py-6 md:px-0 lg:py-8">
               <Page />
             </div>
           </div>
