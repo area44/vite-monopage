323b83a787e7b4901b96b33c5da490c9f56835cd
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2026-02-08 15:35:37 +0700

    feat: update layout

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ export default function App() {
       <div className="bg-background relative z-10 flex min-h-svh flex-col">
         <main className="flex flex-1 flex-col">
           <div className="container-wrapper flex flex-1 flex-col px-2 py-4 lg:py-20">
-            <div className="mx-auto flex w-full max-w-160 min-w-0 flex-1 flex-col gap-6 px-4 py-6 md:px-0 lg:py-8">
+            <div className="mx-auto flex w-full max-w-3xl min-w-0 flex-1 flex-col gap-6 px-4 py-6 md:px-0 lg:py-8">
               <Page />
             </div>
           </div>
